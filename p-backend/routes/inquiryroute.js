ee2bbf085d87c60aad626058e7a72ce2640e1b01
Tanvi <tanvij1609@gmail.com>
2026-05-27 14:41:24 +0530

    Removed old SMTP password config

--- p-backend/routes/inquiryroute.js
+++ p-backend/routes/inquiryroute.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Inquiry = require("../models/Inquiry");
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const axios = require("axios");
 
 router.post("/inquiry", async (req, res) => {
   console.log(req.body);
+
   try {
     const { name, phone, email, message } = req.body;
 
@@ -22,34 +13,57 @@ router.post("/inquiry", async (req, res) => {
       name,
       phone,
       email,
-      message
+      message,
     });
 
     await newInquiry.save();
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
+    await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "Piraka Trading Company",
+          email: process.env.EMAIL_USER,
+        },
 
-      subject: "New Inquiry Received",
+        to: [
+          {
+            email: process.env.EMAIL_USER,
+          },
+        ],
 
-      html: `
-        <h2>New Inquiry</h2>
-        <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Phone:</strong> ${phone}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Message:</strong> ${message}</p>
-      `,
+        subject: "New Inquiry Received",
+
+        htmlContent: `
+          <h2>New Inquiry</h2>
+          <p><strong>Name:</strong> ${name}</p>
+          <p><strong>Phone:</strong> ${phone}</p>
+          <p><strong>Email:</strong> ${email}</p>
+          <p><strong>Message:</strong> ${message}</p>
+        `,
+      },
+
+      {
+        headers: {
+          accept: "application/json",
+          "api-key": process.env.BREVO_API_KEY,
+          "content-type": "application/json",
+        },
+      }
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Inquiry saved",
     });
 
-    res.status(201).json({ success: true, message: "Inquiry saved" });
-  } 
-    catch (error) {
-      console.log(error);
-      res.status(500).json({ error: error.message });
-    }
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      error: error.message,
+    });
+  }
 });
-
-
 
 module.exports = router;
