3347899173779c02544ff6ec1bc69bbaaff3780d
Tanvi <tanvij1609@gmail.com>
2026-05-27 16:15:20 +0530

    Cleaned debug logs

--- p-backend/routes/inquiryroute.js
+++ p-backend/routes/inquiryroute.js
@@ -4,7 +4,6 @@ const Inquiry = require("../models/Inquiry");
 const axios = require("axios");
 
 router.post("/inquiry", async (req, res) => {
-  console.log(req.body);
 
   try {
     const { name, phone, email, message } = req.body;
--- p-frontend/frontend/src/pages/ContactPage.tsx
+++ p-frontend/frontend/src/pages/ContactPage.tsx
@@ -27,7 +27,6 @@ export default function ContactPage() {
     setError('');
 
     try {
-      console.log("API URL:", import.meta.env.VITE_API_URL);
       await axios.post(`${import.meta.env.VITE_API_URL}/api/inquiry`, {
       name: form.name.trim(),
       phone: form.phone.trim(),
